5597536260f9d010287fe64382990ffa2cfea368
David Junker <social.david@hotmail.com>
2022-05-13 16:04:56 +0200

    update: basic user backend

---
+++ src/models/db.js
@@ -0,0 +1,11 @@
+import { userMongoStore } from "./mongo/user-mongo-store.js";
+import { connectMongo } from "./mongo/connect.js";
+
+export const db = {
+  userStore: null,
+
+  init() {
+    this.userStore = userMongoStore;
+    connectMongo();
+  },
+};
