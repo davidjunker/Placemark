0fad637ba05b3e50c1799cbbe32bfbbcceeac143
David Junker <social.david@hotmail.com>
2022-05-18 10:58:29 +0200

    add category and poi mongo db and api

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,17 @@
 import { userMongoStore } from "./mongo/user-mongo-store.js";
+import { categoryMongoStore } from "./mongo/category-mongo-store.js";
+import { poiMongoStore } from "./mongo/poi-mongo-store.js";
 import { connectMongo } from "./mongo/connect.js";
 
 export const db = {
   userStore: null,
+  categoryStore: null,
+  poiStore: null,
 
   init() {
     this.userStore = userMongoStore;
+    this.categoryStore = categoryMongoStore;
+    this.poiStore = poiMongoStore;
     connectMongo();
   },
 };
--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -11,3 +11,14 @@ export const UserSpec = {
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+
+export const PoiSpec = {
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  latitude: Joi.number().allow("").optional(),
+  longitude: Joi.number().allow("").optional(),
+};
+
+export const CategorySpec = {
+  title: Joi.string().required(),
+};
